5756ba604809811e6cebb9169a732f90299a2dd2
Celso Ribeiro <celsofilho82@gmail.com>
2021-03-02 08:50:15 -0300

    Iniciando o projeto

---
+++ src/main/java/com/example/desafioforumalura/DesafioForumAluraApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafioforumalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioForumAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioForumAluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafioforumalura/DesafioForumAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafioforumalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioForumAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
